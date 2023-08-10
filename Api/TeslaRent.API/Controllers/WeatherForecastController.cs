0c87e3f5f34793dea4c23009d2f1a7e79c1b2b2f
Konrad Lubera <konradlubera@hotmail.com>
2023-08-10 13:54:11 +0200

    Renamed api project

--- Api/TeslaRent.API/Controllers/WeatherForecastController.cs
+++ Api/TeslaRent.API/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace Api.Controllers;
+namespace TeslaRent.API.Controllers;
 
 [ApiController]
 [Route("[controller]")]
--- Api/TeslaRent.API/WeatherForecast.cs
+++ Api/TeslaRent.API/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace Api;
+namespace TeslaRent.API;
 
 public class WeatherForecast
 {
