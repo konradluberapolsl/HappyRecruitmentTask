0b02ef20aa119ce4aa85031b177a89d12368b2bb
Konrad Lubera <konradlubera@hotmail.com>
2023-08-23 16:53:58 +0200

    Added mui theme

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.scss'
 import reportWebVitals from './reportWebVitals';
 import App from "./App";
+import {ThemeProvider} from "@mui/material";
+import {theme} from "./constants/ThemeMUI";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App/>
+      <ThemeProvider theme={theme}>
+          <App/>
+      </ThemeProvider>
   </React.StrictMode>
 );
 
