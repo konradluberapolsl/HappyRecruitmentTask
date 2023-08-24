16cdcb1ac46c7d1f2b155f23e1b78b08f1041c49
Konrad Lubera <konradlubera@hotmail.com>
2023-08-24 04:03:09 +0200

    Edited axios client

--- web/src/api/Client.tsx
+++ web/src/api/Client.tsx
@@ -1,9 +1,20 @@
 import axios from "axios";
 import data from "../config.json"
 
-export default axios.create({
+const httpClient = axios.create({
     baseURL: data.ApiUrl,
     headers: {
         "Content-type": "application/json"
     },
 });
+
+export const addInterceptor = (getAccessTokenSilently: any) => {
+    httpClient.interceptors.request.use(async (config) => {
+        const token = await getAccessTokenSilently();
+        config.headers.Authorization = `Bearer ${token}`;
+        config.baseURL = data.ApiUrl;
+        return config;
+    });
+};
+
+export default httpClient;
